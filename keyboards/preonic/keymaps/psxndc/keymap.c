f4e105132188bdbb6169d869449845bcd8253b59
psxndc <github@patrickmyers.net>
2020-01-03 15:35:29 -0800

    updating keymaps

--- keyboards/preonic/keymaps/psxndc/keymap.c
+++ keyboards/preonic/keymaps/psxndc/keymap.c
@@ -70,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_preonic_grid( \
-  KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_SCLN,  \
-  SFT_T(KC_BSPC), KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    RSFT_T(KC_BSPC), \
-  KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_M,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  KC_LCTL,        KC_LALT, KC_LGUI, KC_APP,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,          KC_7,  KC_8,    KC_9,    KC_0,    KC_DEL, \
+  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,           KC_J,          KC_L,  KC_U,    KC_Y,    KC_QUOT, KC_SCLN, \
+  KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,           KC_K,          KC_N,  KC_E,    KC_I,    KC_O,    KC_BSPC, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,           KC_M,          KC_H,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
+  KC_LCTL, KC_LALT, KC_LGUI, KC_APP,  LOWER,   LSFT_T(KC_SPC), LSFT_T(KC_SPC),RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 
@@ -93,10 +93,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC, \
-  KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, KC_PSCR, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PIPE,  \
-  _______, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS, KC_EQL,  KC_NLCK, KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_MUTE, \
-  _______, KC_RCBR, KC_RBRC, KC_RPRN, KC_UNDS, KC_PLUS, _______, KC_P1,   KC_P2,   KC_P3,   KC_BSLS, KC_MPLY , \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, KC_PSCR, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PIPE, \
+  _______, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS, KC_PLUS, KC_NLCK, KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_MUTE, \
+  _______, KC_RCBR, KC_RBRC, KC_RPRN, KC_UNDS, KC_EQL,  _______, KC_P1,   KC_P2,   KC_P3,   KC_BSLS, KC_MPLY, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
 /* Raise
@@ -134,12 +134,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid( \
-  _______, _______, _______, _______, _______, _______, _______, _______,  _______,_______, _______, RESET,  \
-  _______, _______, _______, _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, DEBUG,  \
+  _______, _______, _______, _______, _______, _______, _______, _______,  _______,_______, _______, RESET,    \
+  _______, _______, _______, _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, DEBUG,    \
   _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, _______,  _______, _______, \
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD , \
-  _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAD, RGB_SAD, RGB_HUD, RGB_TOG  \
-)
+  _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAD, RGB_SAD, RGB_HUD, RGB_TOG   \
+),
 
 
 };
