9ab4e87c07b141b7fba351c464dd72fb1216ad57
Patrick Myers <git@patrickmyers.net>
2020-07-14 16:43:57 -0700

    updating keymaps for user psxndc

--- keyboards/planck/keymaps/psxndc/keymap.c
+++ keyboards/planck/keymaps/psxndc/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
-  KC_TAB,           KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,   KC_L,  KC_U,    KC_Y,    KC_QUOT, KC_BSPC, \
+  KC_TAB,           KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,   KC_L,  KC_U,    KC_Y,    KC_QUOT, KC_DEL, \
   LCTL_T(KC_BSPC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,   KC_N,  KC_E,    KC_I,    KC_O,    RALT_T(KC_SCLN), \
   KC_LSFT, 			KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_M,   KC_H,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
   KC_LCTL, 			KC_LGUI, KC_LALT, MO(_WRD),LOWER,   KC_SPC,  KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT   \
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-   KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, KC_PSCR, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_DEL, \
+   KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, KC_PSCR, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_BSPC, \
    KC_TILD, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS, KC_PLUS, KC_NLCK, KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_PIPE, \
    _______, KC_RCBR, KC_RBRC, KC_RPRN, KC_UNDS, KC_EQL,  _______, KC_P1,   KC_P2,   KC_P3,   KC_BSLS, _______, \
    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
