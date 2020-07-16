0ab59fc864b7541e99c518a7fe9993901ddd0ae7
Patrick Myers <github@patrickmyers.net>
2020-07-16 13:02:21 -0700

    updating keymaps for user psxndc

--- keyboards/planck/keymaps/psxndc/keymap.c
+++ keyboards/planck/keymaps/psxndc/keymap.c
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
-  KC_TAB,           KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,   KC_L,  KC_U,    KC_Y,    KC_QUOT, KC_DEL, \
-  LCTL_T(KC_BSPC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,   KC_N,  KC_E,    KC_I,    KC_O,    RALT_T(KC_SCLN), \
+  KC_TAB,           KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,   KC_L,  KC_U,    KC_Y,    KC_QUOT, KC_BSPC, \
+  LCTL_T(KC_DEL),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_K,   KC_N,  KC_E,    KC_I,    KC_O,    RALT_T(KC_SCLN), \
   KC_LSFT, 			KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_M,   KC_H,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
   KC_LCTL, 			KC_LGUI, KC_LALT, MO(_WRD),LOWER,   KC_SPC,  KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT   \
 ),
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-   KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, KC_PSCR, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_BSPC, \
+   KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, KC_PSCR, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_DEL, \
    KC_TILD, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS, KC_PLUS, KC_NLCK, KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_PIPE, \
    _______, KC_RCBR, KC_RBRC, KC_RPRN, KC_UNDS, KC_EQL,  _______, KC_P1,   KC_P2,   KC_P3,   KC_BSLS, _______, \
    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
+   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL, \
    KC_MINS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_COLN, \
    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, KC_MUTE, KC_MPLY, \
    _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT  \
@@ -145,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  [_WRD] = LAYOUT_planck_grid( \
   _______, _______, _______, _______, _______, _______,_______, KC_P7        , S(KC_UP)  , KC_P9        , _______, _______, \
-  _______, _______, _______, _______, _______, _______,_______, C(S(KC_LEFT)), KC_P5     , C(S(KC_RGHT)), _______, _______, \
+  _______, _______, _______, _______, KC_LGUI, _______,_______, C(S(KC_LEFT)), KC_P5     , C(S(KC_RGHT)), _______, _______, \
   _______, _______, _______, _______, _______, _______,_______, KC_P1        , S(KC_DOWN), KC_P3        , _______, _______, \
   _______, _______, _______, _______, _______, _______,_______, _______      , _______   , _______      , _______, _______  \
  ),
--- keyboards/preonic/keymaps/psxndc/rules.mk
+++ keyboards/preonic/keymaps/psxndc/rules.mk
@@ -1,5 +1,6 @@
 SRC += muse.c
 
+TAP_DANCE_ENABLE = yes
 AUTO_SHIFT_ENABLE = no
 AUDIO_ENABLE = yes
 
