de32f32a124d4d20a617006cbaec0745e4f6aef3
psxndc <github@patrickmyers.net>
2021-03-22 01:23:31 -0700

    updating keymaps for user psxndc

--- keyboards/walletburner/cajal/keymaps/psxndc/keymap.c
+++ keyboards/walletburner/cajal/keymaps/psxndc/keymap.c
@@ -26,31 +26,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_ortho(
         KC_TAB,   KC_Q,    		KC_W,   	 KC_F,    	  KC_P,    	   KC_B,    KC_J,   	KC_L,  		 KC_U,    	  KC_Y,		    KC_QUOT, 	 KC_BSPC,  KC_DEL, RGB_TOG,
-        KC_DEL,	  KC_A,   		LALT_T(KC_R),LSFT_T(KC_S),LCTL_T(KC_T),KC_G,    KC_K,   	RCTL_T(KC_N),RSFT_T(KC_E),RALT_T(KC_I), KC_O,		 KC_SCLN,  KC_ENT,
-        KC_LSFT,  KC_Z,    		KC_X, 		 KC_C,    	  KC_D,    	   KC_V,    KC_M,   	KC_H,  		 KC_COMM,	  KC_DOT,  		KC_SLSH, 	 KC_ENT,   KC_UP
-        KC_LCTL,  KC_LGUI, 		KC_LALT, 	 WORD, 		  LOWER,   	   KC_SPC, 	KC_SPC,		RAISE,		 KC_RALT, 	  KC_RGUI, 		KC_CTRL,     KC_LEFT,  KC_DOWN, KC_RGHT 
+        KC_DEL,	  KC_A,   		LALT_T(KC_R),LSFT_T(KC_S),LCTL_T(KC_T),KC_G,    KC_K,   	RCTL_T(KC_N),RSFT_T(KC_E),RALT_T(KC_I), KC_O,		 KC_SCLN,  KC_INS,
+        KC_LSFT,  KC_Z,    		KC_X, 		 KC_C,    	  KC_D,    	   KC_V,    KC_M,   	KC_H,  		 KC_COMM,	  KC_DOT,  		KC_SLSH, 	 KC_ENT,   KC_UP,
+        KC_LCTL,  KC_LGUI, 		KC_LALT, 	 WORD, 		  LOWER,   	   KC_SPC, 	KC_SPC,		RAISE,		 KC_RALT, 	  KC_RGUI, 		KC_RCTL,     KC_LEFT,  KC_DOWN, KC_RGHT 
 	),
 //LOWER
     [1] = LAYOUT_ortho(
-        KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, KC_PSCR, KC_P7,   KC_P8,   KC_P9,   KC_P0,   _______,  KC_DEL, RGB_MOD,
+        KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______, KC_PSCR, KC_P7,   KC_P8,   KC_P9,   KC_P0,   _______,  _______, RGB_MOD,
         KC_TILD, KC_LCBR, KC_LBRC, KC_LPRN, KC_MINS, KC_PLUS, _______, KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_PIPE,  _______,
         _______, KC_RCBR, KC_RBRC, KC_RPRN, KC_UNDS, KC_EQL,  _______, KC_P1,   KC_P2,   KC_P3,   KC_BSLS, _______,  KC_PGUP,
         _______, _______, _______, _______, _______,  _______,_______, ADJUST,_______,  _______, _______,  KC_HOME,  KC_PGDN, KC_END
     ),
 //RAISE
     [2] = LAYOUT_ortho(
-		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______
-		KC_MINS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_COLN, _______
+		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______,
+		KC_MINS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_COLN, _______,
 		_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, KC_MUTE, KC_MPLY, KC_VOLU, 
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT,   
+		_______, _______, _______, _______, ADJUST,  _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT   
     ),
 //ADJUST
     [3] = LAYOUT_ortho(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI,  RESET,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAD,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,	RGB_HUI
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,	RGB_HUI,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SAD, RGB_HUD, RGB_SAI
-    )
+    ),
 /* Useful commands for MS Word
  * ,---------------------------------------------------------------------------------------------------------.
  * |      |  1   |  2   |   3  |  4   |  5   |  6   |   7            |    8    |   9           |  0   |      |
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[4] = LAYOUT_ortho(
 /*,-----------------------------------------------------------------------------------------------------------------------.
  *|      |   1    |    2   |    3   |   4    |   5    |   6   |   7          |     8     |    9         |   0    |        |*/ 
-  _______, _______, _______, _______, _______, _______,_______, KC_P7        , S(KC_UP)  , KC_P9        , _______, _______, _______,
-  _______, _______, _______, _______, KC_LGUI, _______,_______, C(S(KC_LEFT)), KC_P5     , C(S(KC_RGHT)), _______, _______, 
-  _______, _______, _______, _______, _______, _______,_______, KC_P1        , S(KC_DOWN), KC_P3        , _______, _______, 
-  _______, _______, _______, _______, _______, _______,_______, _______      , _______   , _______      , _______, _______  _______,
-  )
+  _______, _______, _______, _______, _______, _______,_______, KC_P7        , S(KC_UP)  , KC_P9        , _______, _______,_______, _______,
+  _______, _______, _______, _______, KC_LGUI, _______,_______, C(S(KC_LEFT)), KC_P5     , C(S(KC_RGHT)), _______, _______,_______, 
+  _______, _______, _______, _______, _______, _______,_______, KC_P1        , S(KC_DOWN), KC_P3        , _______, _______,_______, 
+  _______, _______, _______, _______, _______, _______,_______, _______      , _______   , _______      , _______, _______,_______, _______
+  ),
 };
 
 
